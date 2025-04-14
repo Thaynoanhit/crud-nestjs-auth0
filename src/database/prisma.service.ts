70e72dbede4099cbc6dab1f3e692dd4889ca9567
thaynoanhit <hitab2000@gmail.com>
2025-04-14 12:23:42 -0300

    alterações na logica de hashing

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,9 +1,16 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+    this.$use((params, next) => {
+      if (params.model === 'User' && ['create', 'update'].includes(params.action) && params.args.data['password']) {
+        params.args.data['password'] = bcrypt.hashSync(params.args.data['password'], 10)
+      }
+      return next(params)
+    })
   }
 }
